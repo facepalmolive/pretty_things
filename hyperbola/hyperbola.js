785b35c4f02c67d777c8bf7acec1d383fa595af1
facepalmolive <3971705+facepalmolive@users.noreply.github.com>
2018-02-07 16:22:43 -0800

    Refactored camera stuff into own module and updated hyperbola.js accordingly.

--- hyperbola/hyperbola.js
+++ hyperbola/hyperbola.js
@@ -1,44 +1,22 @@
-var hyperbola = {
-    MAX: 60,
-    COLOR_FROM: new util.color.Color(0x0000ff),
-    COLOR_TO: new util.color.Color(0xff00ff),
-
-    init: function(canvasWidth, canvasHeight) {
-        var renderer = new THREE.WebGLRenderer();
-        renderer.setSize(canvasWidth, canvasHeight);
-
-        var ratio = canvasWidth / canvasHeight;
-        var camera = new THREE.OrthographicCamera(
-                -this.MAX * ratio, this.MAX * ratio,
-                this.MAX, -this.MAX,
-                -this.MAX, this.MAX);
-        var scene = new THREE.Scene();
-
-        this.renderer = renderer;
-        this.camera = camera;
-        this.scene = scene;
-    },
-
-    render: function() {
-        this.renderer.render(this.scene, this.camera);
-    },
-
-    drawLines: function() {
-        for (var i = 0; i < this.MAX + 1; i++) {
+var hyperbola = (function(THREE, util) {
+    var exported = {};
+    exported.drawLines = function(scene, color_from, color_to, size) {
+        for (var i = 0; i < size + 1; i++) {
             var geometry = new THREE.Geometry();
             geometry.vertices.push(new THREE.Vector3(0, i, 0));
-            geometry.vertices.push(new THREE.Vector3(this.MAX - i, 0, 0));
+            geometry.vertices.push(new THREE.Vector3(size - i, 0, 0));
             geometry.vertices.push(new THREE.Vector3(0, -i, 0));
-            geometry.vertices.push(new THREE.Vector3(-(this.MAX - i), 0, 0));
+            geometry.vertices.push(new THREE.Vector3(-(size - i), 0, 0));
             geometry.vertices.push(new THREE.Vector3(0, i, 0));
 
-            var gradient = i / this.MAX;
+            var gradient = i / size;
             var material = new THREE.LineBasicMaterial({
-                color: this.COLOR_FROM.scale(1 - gradient).hex() +
-                       this.COLOR_TO.scale(gradient).hex(),
+                color: color_from.scale(1 - gradient).hex() +
+                       color_to.scale(gradient).hex(),
             });
 
-            this.scene.add(new THREE.Line(geometry, material));
+            scene.add(new THREE.Line(geometry, material));
         }
-    }
-};
+    };
+    return exported;
+}(THREE, util));
